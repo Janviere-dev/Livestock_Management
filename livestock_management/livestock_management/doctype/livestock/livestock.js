720380c07b91809fe1dc6384c8718817a374d16d
Janviere-dev <j.munezero@alustudent.com>
2026-03-20 23:44:09 +0200

    Feat: Add custom button for opening a dialog for reason for the termination. If the reason is sold, It must open hidden field of customer, and selling price.

--- livestock_management/livestock_management/doctype/livestock/livestock.js
+++ livestock_management/livestock_management/doctype/livestock/livestock.js
@@ -16,7 +16,69 @@ frappe.ui.form.on("Livestock", {
             }
         })
 
-
-
+        // Custom Button "Analog activity"
+        
+        if (!frm.is_new() && frm.doc.status === "Active"){
+            frm.add_custom_button(("Log Activity"), () => {
+                open_log_activity_dialog(frm)
+            });
+        }
 	},
 });
+
+function open_log_activity_dialog(frm) {
+    dialog = new frappe.ui.Dialog({
+        title: "Log Activity",
+        fields: [
+            {
+                label: "Reason for Termination",
+                fieldname: "reason_for_termination",
+                fieldtype: "Select",
+                options: ["Sold", "Dead", "Missing", "Slaughtered"],
+                reqd: 1,
+                onchange: () => toggle_sold_fields(dialog),
+            },
+
+            {
+                fieldtype: "Link",
+                fieldname: "customer",
+                label: "Customer",
+                options: "Customer",
+                hidden: 1,
+            },
+
+            {
+
+                fieldtype: "Currency",
+                fieldname: "selling_price",
+                label: "Selling Price",
+                hidden: 1,
+            },
+
+        ],
+        primary_action_label:"Submit",
+        primary_action(values) {
+            reason = values.reason_for_termination;
+
+            if (reason === "Sold" && (!values.customer || !values.selling_price)){
+                frappe.msgprint(("Customer and Selling Price are required when reason is Sold."));
+                return;
+
+            }
+            frm.set_value("status", reason);
+            frm.save();
+            dialog.hide();
+        },
+    });
+    dialog.show();
+}
+
+function toggle_sold_fields(dialog) {
+    reason = dialog.get_value("reason_for_termination");
+    is_sold = reason === "Sold";
+
+    dialog.set_df_property("customer", "hidden", !is_sold);
+    dialog.set_df_property("selling_price", "hidden", !is_sold);
+    dialog.set_df_property("customer", "reqd", is_sold);
+    dialog.set_df_property("selling_price", "reqd", is_sold);
+}
